969c63174d2fff9c20cacf6fed7eb99429634ab9
Nikita Ravi <ravi30@gilbreth-fe03.rcac.purdue.edu>
2024-01-13 16:00:17 -0500

    initialized seed

--- TRUNK/main.py
+++ TRUNK/main.py
@@ -13,8 +13,16 @@ import time
 from omegaconf import OmegaConf
 
 # Global Variables
+config = get_hyperparameters(f"./Datasets/{args.dataset.lower()}/{args.model_backbone.lower()}")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Device is on {device} for main.py")
+
+## set seed
+torch.manual_seed(config.seed)
+if(device == "cuda"):
+    torch.cuda.manual_seed(config.seed)
+
+## move everything to gpu
 device = torch.device(device)
 
 def parser():
@@ -244,7 +252,6 @@ def format_time(runtime):
 def main():
     start_time = time.time()
     args = parser()
-    config = get_hyperparameters(f"./Datasets/{args.dataset.lower()}/{args.model_backbone.lower()}")
 
     if(args.improve_model_weights):
         ### Improving the validation accuracy of a trained module
